c2b68a0b12f888a42b16b3d2343cdd078cc0eb5d
Harshitasingh12896 <shikhasingh70869@gmail.com>
2025-02-15 23:26:14 +0530

    first commit

--- config/database.ts
+++ config/database.ts
@@ -2,12 +2,14 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'), // Replace with your MySQL host
-      port: env.int('DATABASE_PORT', 3306),    // Replace with your MySQL port
-      database: env('DATABASE_NAME', 'strapi'), // Replace with your database name
-      user: env('DATABASE_USERNAME', 'root'),  // Replace with your MySQL username
-      password: env('DATABASE_PASSWORD', 'Harshita@2003'), // Replace with your MySQL password
-      ssl: env.bool('DATABASE_SSL', false),    // Set to true if SSL is required
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', 'Harshita@2003'), // Corrected line
+      ssl: env.bool('DATABASE_SSL', false),
     },
+    pool: { min: 0, max: 5 }, // Prevent max connections issue
   },
 });
+
