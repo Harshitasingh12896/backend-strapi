72861fd4d8017c65c7c2de84866225f3a79d8dcf
Harshitasingh12896 <shikhasingh70869@gmail.com>
2025-02-16 16:37:52 +0530

    Fix: Increased MySQL connection pool size for deployment stability

--- config/database.ts
+++ config/database.ts
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
       password: env('DATABASE_PASSWORD', 'Harshita@2003'), // Corrected line
       ssl: env.bool('DATABASE_SSL', false),
     },
-    pool: { min: 0, max: 5 }, // Prevent max connections issue
+    pool: { min: 2, max: 10 }, // Prevent max connections issue
   },
 });
 
