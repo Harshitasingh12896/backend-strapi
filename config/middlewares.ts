f46915e238d9aa1ed9dff13aa9ac5ef943374344
Harshitasingh12896 <shikhasingh70869@gmail.com>
2025-02-17 15:27:19 +0530

    update middleware.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -12,7 +12,8 @@ export default [
             "blob:",
             "*.cloudinary.com",
             "https://backend-strapi-8.onrender.com",
-            "https://nextjs-project-five-indol.vercel.app/", // ✅ Allow Vercel frontend
+            "https://nextjs-project-five-indol.vercel.app", // ✅ Vercel frontend
+            "http://localhost:3000", // ✅ Allow localhost
           ],
           "media-src": [
             "'self'",
@@ -20,7 +21,8 @@ export default [
             "blob:",
             "*.cloudinary.com",
             "https://backend-strapi-8.onrender.com",
-            "https://nextjs-project-five-indol.vercel.app/", // ✅ Allow Vercel frontend
+            "https://nextjs-project-five-indol.vercel.app", // ✅ Vercel frontend
+            "http://localhost:3000", // ✅ Allow localhost
           ],
         },
       },
@@ -29,7 +31,10 @@ export default [
   {
     name: "strapi::cors",
     config: {
-      origin: [ "https://nextjs-project-five-indol.vercel.app/"], // ✅ Set allowed origins
+      origin: [
+        "https://nextjs-project-five-indol.vercel.app", // ✅ Vercel frontend
+        "http://localhost:3000", // ✅ Allow localhost
+      ], // ✅ Set allowed origins
       methods: ["GET", "POST", "PUT", "DELETE"],
       allowedHeaders: ["Content-Type", "Authorization"],
       credentials: true, // ✅ Allow credentials
